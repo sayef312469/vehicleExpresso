b0330e43480432d8d688336c0920ba780fb933f9
Saief Md. Hossain Adnan <125680254+saiefadnan@users.noreply.github.com>
2024-06-21 00:25:24 +0600

    Update SearchPaking.js

--- frontend/src/pages/SearchPaking.js
+++ frontend/src/pages/SearchPaking.js
@@ -1,19 +1,27 @@
-import { useParksContext } from "../hooks/useParksContext";
+import { useParksContext } from '../hooks/useParksContext'
 
-import SearchForm from "../components/SearchForm";
-import ParkDetails from "../components/ParkInfo";
+import React from 'react'
+import ParkDetails from '../components/ParkInfo'
+import SearchForm from '../components/SearchForm'
 
 const SearchParking = () => {
-  const { parks, dispatch } = useParksContext();
+  const { parks } = useParksContext()
 
   return (
     <div className="searchParking">
       <SearchForm />
       <div className="parks">
-        {parks && parks.map((park) => <ParkDetails park={park} />)}
+        {parks &&
+          parks.map((park) => (
+            <ParkDetails
+              park={park}
+              key={park}
+            />
+          ))}
+        {!parks && <h4>No Parks</h4>}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default SearchParking;
+export default SearchParking
