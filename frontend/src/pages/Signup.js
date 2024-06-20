65f17d5564b8de225ee696723050bd44f4e2fb60
Saief Md. Hossain Adnan <125680254+saiefadnan@users.noreply.github.com>
2024-06-21 00:26:28 +0600

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,50 +1,74 @@
-import { useState } from "react";
-import { useSignup } from "../hooks/useSignup";
+import React, { useState } from 'react'
+import { useSignup } from '../hooks/useSignup'
 
 const Signup = () => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [conPassword, setConPassword] = useState("");
-  const {signup, error, isLoading} = useSignup();
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [conPassword, setConPassword] = useState('')
+  const { signup, error, isLoading } = useSignup()
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    await signup(username, email, password, conPassword);
-  };
+    await signup(username, email, password, conPassword)
+  }
 
   return (
-    <form className="signup" onSubmit={handleSubmit}>
+    <form
+      className="signup"
+      onSubmit={handleSubmit}
+    >
       <h3>Sign Up</h3>
-      <label>Username:</label>
-      <input
-        type="text"
-        onChange={(e) => setUsername(e.target.value)}
-        value={username}
-      />
-      <label>Email:</label>
-      <input
-        type="email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      />
-      <label>Password:</label>
-      <input
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      />
-      <label>Confirm Password:</label>
-      <input
-        type="password"
-        onChange={(e) => setConPassword(e.target.value)}
-        value={conPassword}
-      />
+      <hr />
+      <div className="input_box">
+        <label>
+          <span className="material-symbols-outlined">verified_user</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+      </div>
+      <div className="input_box">
+        <label>
+          <span className="material-symbols-outlined">alternate_email</span>
+        </label>
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </div>
+      <div className="input_box">
+        <label>
+          <span className="material-symbols-outlined">lock_open</span>
+        </label>
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </div>
+      <div className="input_box">
+        <label>
+          <span className="material-symbols-outlined">enhanced_encryption</span>
+        </label>
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          onChange={(e) => setConPassword(e.target.value)}
+          value={conPassword}
+        />
+      </div>
       <button disabled={isLoading}>Sign Up</button>
       {error && <div className="error">{error}</div>}
     </form>
-  );
-};
+  )
+}
 
-export default Signup;
+export default Signup
