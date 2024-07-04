20247eaf731c4a535f75de05cfef0d01ab36a03e
sayef312469 <202214069@student.mist.ac.bd>
2024-07-05 00:39:31 +0600

    Updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,6 @@ import UserParkHistory from './pages/UserParkHistory'
 import VehicleCare from './pages/VehicleCare'
 import VehicleEntryExit from './pages/VehicleEntryExit'
 import Profile from './pages/userProfile'
-import ProfileTest from './pages/usertest'
 import RecordHistory from './pages/record'
 
 function App() {
