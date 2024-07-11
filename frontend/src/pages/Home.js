45e38d76f72a1da01f060a7ce774129f416e0479
Tamal267 <syedtamal@gmail.com>
2024-07-11 10:16:06 +0600

    edited homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import SmallCardServices from '../components/SmallCardServices'
+import s1 from '../img/s1.png'
+import s2 from '../img/s2.png'
+import s3 from '../img/s3.png'
+import s4 from '../img/s4.png'
+import s5 from '../img/s5.png'
 
 const Home = () => {
+  const serviceArr = [
+    { im: s1, btn: 'Vehicle Parks' },
+    { im: s2, btn: 'Search Parks' },
+    { im: s3, btn: 'Care Vehicle' },
+    { im: s4, btn: 'Track Activity' },
+    { im: s5, btn: 'Add Vehicle' },
+  ]
   return (
     <div className="home">
       <Container
@@ -18,7 +30,16 @@ const Home = () => {
           </p>
         </div>
         <div className="rowServices">
-          <SmallCardServices />
+          <div className="heroServices">
+            {serviceArr &&
+              serviceArr.map((s) => (
+                <SmallCardServices
+                  key={s.btn}
+                  im={s.im}
+                  btn={s.btn}
+                />
+              ))}
+          </div>
         </div>
       </Container>
     </div>
